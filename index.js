93d7178743bcf054c226cd9ef4ad7688a8695f8d
maoshuochen <maoshuochen@gmail.com>
2021-03-06 15:29:42 +0800

    index > main-panel

--- index.js
+++ index.js
@@ -46,7 +46,14 @@ var groupMenu = Vue.createApp({
                 {
                     index: 3,
                     text: 'Knowledge Graph',
-                    image: './assets/side-panel/arrow-right.svg'
+                    image: './assets/side-panel/arrow-right.svg',
+                    tags: [
+                        { value: 'Knowledge Graph', type: 1 },
+                        { value: 'Nature Language Processing', type: 1 },
+                        { value: 'Database', type: 1 },
+                        { value: 'Cited Index', type: 1 },
+                        { value: 'Omar Alonso', type: 2 }
+                    ]
                 },
                 {
                     index: 2,
@@ -58,24 +65,42 @@ var groupMenu = Vue.createApp({
     }
 })
 
+// group1: Knowledge Graph
+// group2: Data Vis
+// group3: Others
+
 var articlesGroup = Vue.createApp({
     data() {
         return {
             articles: [{
                     index: 1,
+                    group: 3,
                     type: "Journal Article",
                     author: "Andy Extance",
                     date: "October 2019",
                     position: "Nature-VOL 56-Toolbox",
                     title: 'How AI Technology can tame the scientific literature',
-                    description: 'description of article1',
-                    image: ''
+                    abstract: 'As artificially intelligent tools for literature and data exploration evolve, developers seek to automate how hypotheses are generated and validated.',
+                    image: './assets/main-panel/article-images/article1.png',
+                    keywords: ['AI-based', 'Nature Language Processing', 'Knowledge Graph'],
+                    cite: 2,
+                    comment: 15,
+                    trend: 50
                 },
                 {
                     index: 2,
-                    title: 'How AI Technology can tame the scientific literature',
-                    description: 'description of article2',
-                    image: ''
+                    group: 3,
+                    type: "Journal Article",
+                    author: "Marco Cavallo",
+                    date: "January 2019",
+                    position: "IEEE Transactions on Visualization and Computer Graphics",
+                    title: 'Clustrophile 2: Guided Visual Clustering Analysis',
+                    abstract: "Data clustering is a common unsupervised learning method frequently used in exploratory data analysis. However, identifying relevant structures in unlabeled, high-dimensional data is nontrivial, requiring iterative experimentation with clustering parameters as well as data features and instances. The number of possible clusterings for a typical dataset is vast, and navigating in this vast space is also challenging. The absence of ground-truth labels makes it impossible to define an optimal solution, thus requiring user judgment to establish what can be considered a satisfiable clustering result. Data scientists need adequate interactive tools to effectively explore and navigate the large clustering space so as to improve the effectiveness of exploratory clustering analysis. We introduce Clustrophile 2, a new interactive tool for guided clustering analysis. Clustrophile 2 guides users in clustering-based exploratory analysis, adapts user feedback to improve user guidance, facilitates the interpretation of clusters, and helps quickly reason about differences between clusterings. To this end, Clustrophile 2 contributes a novel feature, the Clustering Tour, to help users choose clustering parameters and assess the quality of different clustering results in relation to current analysis goals and user expectations. We evaluate Clustrophile 2 through a user study with 12 data scientists, who used our tool to explore and interpret sub-cohorts in a dataset of Parkinson's disease patients. Results suggest that Clustrophile 2 improves the speed and effectiveness of exploratory clustering analysis for both experts and non-experts.",
+                    image: './assets/main-panel/article-images/article2.png',
+                    keywords: ['Clustering Tour', 'Guided Data Analysis', 'Unsupervised Learning'],
+                    cite: 3,
+                    comment: 52,
+                    trend: 60
                 }
             ]
         }
